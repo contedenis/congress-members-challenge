ed89344cd9454c18084d33e22ccce1f2a4f449ef
contedenis <contedenis@outlook.com>
2019-12-08 22:16:04 -0300

    feat/add-datatable-to-main-and-styles

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,12 +2,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+// @app
+import DataTable from '../DataTable';
+
+// @own
+import { members } from './mockData';
+import './styles.scss';
+
 function Main() {
   const { id } = useParams();
 
+  const headItems = [
+    'Title',
+    'Name',
+    'Birth',
+    'Gender',
+    'Party',
+  ];
+
   return (
-    <div>
+    <div className="main">
       {id ? `Main with params ${id}` : 'Main'}
+      <DataTable headItems={headItems} items={members} />
     </div>
   );
 }
