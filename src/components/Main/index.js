c9351deb8dd6a1b40762ef81ae290bd2a27188a7
contedenis <contedenis@outlook.com>
2019-12-10 17:58:52 -0300

    feat/first-step-to-filter-table

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,17 +1,33 @@
 // @packages
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // @app
 import DataTable from '../DataTable';
 import { paginator } from '../../utils/functions';
+import { selectSearchTerm } from '../InputSearch/selectors';
 
 // @own
 import { members } from './mockData';
 import './styles.scss';
 
-function Main() {
+function Main({ searchTerm }) {
   const [page, setPage] = useState(0);
+
+  const congressPersons = {
+    items: paginator(members, 7),
+    totalItems: members.length,
+  };
+
+  function filterTable() {
+  }
+
+  useEffect(() => {
+    filterTable();
+  }, [searchTerm]);
+
   const { id } = useParams();
 
   const headItems = [
@@ -21,11 +37,6 @@ function Main() {
     'Gender',
     'Party',
   ];
-
-  const congressPersons = {
-    items: paginator(members, 7),
-    totalItems: members.length,
-  };
 
   const { items, totalItems } = congressPersons;
 
@@ -44,4 +55,16 @@ function Main() {
   );
 }
 
-export default Main;
+Main.defaultProps = {
+  searchTerm: '',
+};
+
+Main.propTypes = {
+  searchTerm: PropTypes.string,
+};
+
+const mapStateToProps = (state) => ({
+  searchTerm: selectSearchTerm(state),
+});
+
+export default connect(mapStateToProps, null)(Main);
